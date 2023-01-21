1a44c57d8caa6ccf9e4fab70b3a3e46e1c2638ef
rupeshkumar <sprupeshkumar@gmail.com>
2023-01-21 13:08:08 +0530

    Add project files.

---
+++ ProgramAssignWebAPI/Models/Domain/ProgramsTracker.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgramAssignWebAPI.Models.Domain
+{
+    public class ProgramsTracker
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public string TechTrack { get; set; }
+        public string Category { get; set; }
+        public string Program { get; set; }
+    }
+}
---
+++ ProgramAssignWebAPI/Models/Domain/ResourceMangerAssignments.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProgramAssignWebAPI.Models.Domain
+{
+    public class ResourceMangerAssignments
+    {
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int Id { get; set; }
+            public int VAMID { get; set; }
+            public string TechTrack { get; set; }
+            public string ResourceName { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public string Manager { get; set; }
+            public string SME { get; set; }
+            public string SMEStatus { get; set; }
+            public string ProgramStatus { get; set; }
+        // FK Key For 
+        public int ProgramsTrackerId { get; set; }
+        // Navigation prop
+        // Binding navigation prop back to the region
+        public ProgramsTracker ProgramsTracker { get; set; }
+        // navigation prop for WalkDifficulty
+
+
+    }
+}
