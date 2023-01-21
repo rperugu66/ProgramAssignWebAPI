c9017d14d63b2c075383f7f850e49b81d38d500c
Pankaj <Panka@LAPTOP-5VA5SFFR>
2023-01-21 15:09:15 +0530

    Commit 2

--- ProgramAssignWebAPI/Data/AssignDbContext.cs
+++ ProgramAssignWebAPI/Data/AssignDbContext.cs
@@ -3,13 +3,15 @@ using ProgramAssignWebAPI.Models.Domain;
 
 namespace ProgramAssignWebAPI.Data
 {
-    public class AssignDbContext :DbContext
+    public class AssignDbContext : DbContext
     {
         public AssignDbContext(DbContextOptions<AssignDbContext> options) : base(options)
         {
+        }
             // Create Dbset Props to Create Resp Table in the SQL Server
                 public DbSet<ResourceMangerAssignments> ResourceMangerAssignments { get; set; }
-                public DbSet<ProgramsTracker> ProgramsTracker { get; set; }
+        public DbSet<ProgramsTracker> ProgramsTracker { get; set; }
     }
-    }
+    
+    
 }
